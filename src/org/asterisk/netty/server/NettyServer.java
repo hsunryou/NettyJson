a26cd58c1e460ddce5057ae167f3b76d2a9c7ac5
hsunryou <hsunryou@naver.com>
2020-06-27 22:30:59 +0900

    # Commit from IntelliJ

--- src/org/asterisk/netty/server/NettyServer.java
+++ src/org/asterisk/netty/server/NettyServer.java
@@ -1,11 +1,23 @@
 package org.asterisk.netty.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 
 public class NettyServer {
+
+    private EventLoopGroup bossGroup = null;
+    private EventLoopGroup  workerGroup = null;
+    private ServerBootstrap bootstrap = null;
+    private ChannelFuture future = null;
 
     private static final Logger     _Logger = LoggerFactory.getLogger(NettyServer.class);
     public NettyServer() {
@@ -18,19 +30,38 @@ public class NettyServer {
     }
     
     
-    public void startService(){
+    public void startService(int port){
         try{
             _Logger.error( "-- startService ");
-            
+            bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class) // (3)
+                .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                    @Override
+                    public void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new ServerHandler());
+                    }
+                })
+                .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            // Bind and start to accept incoming connections.
+            future = bootstrap.bind(port).sync(); // (7)
+
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            future.channel().closeFuture().sync();
         }catch(Exception ex){
             _Logger.error( "* startService Exception:", ex );
         }
     }
+
     public void stopService(){
         try{
             _Logger.error( "-- stopService ");
-            
-            
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
         }catch(Exception ex){
             _Logger.error( "* stopService Exception:", ex );
         }
---
+++ src/org/asterisk/netty/server/ServerHandler.java
@@ -0,0 +1,60 @@
+package org.asterisk.netty.server;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.concurrent.EventExecutorGroup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger _Logger = LoggerFactory.getLogger(ServerHandler.class);
+    public ServerHandler() {
+        super();
+        try{
+            _Logger.error( "----    ServerHandler    ----");
+
+        }catch(Exception ex){
+            _Logger.error( "* ServerHandler Exception:", ex );
+        }
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        super.channelInactive(ctx);
+        try{
+
+        }catch(Exception ex){
+            _Logger.error( "* channelInactive Exception:", ex );
+        }
+    }
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+        try{
+
+        }catch(Exception ex){
+            _Logger.error( "* channelRead Exception:", ex );
+        }
+    }
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+        try{
+
+        }catch(Exception ex){
+            _Logger.error( "* channelActive Exception:", ex );
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+        try{
+
+        }catch(Exception ex){
+            _Logger.error( "* exceptionCaught Exception:", ex );
+        }
+    }
+}
